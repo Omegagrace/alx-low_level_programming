da7ff201bd165ead388b8e0032cacd8c3afa55de
Omegagrace <pro.egbebi@gmail.com>
2022-12-11 04:53:59 -0800

    task9 10 12 2022

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-
 /**
  * main - prints all combination of single digits
+ *
  * Return: Always 0
  */
-int main(void)
-{
-	int n;
 
-	for (n = 48; n < 58; n++)
+int main(void)
 	{
-		putchar(n);
-		if (n != 57)
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 	}
-}

ec7734b2c017b881bdf851144b6264dd327cf583
Omegagrace <pro.egbebi@gmail.com>
2022-12-14 08:15:09 -0800

    task7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,14 @@
 /**
  * print_last_digit - prints the last digit
  * @n: the number
+ * @ld: is the last digit
  * Return: value of the last digit
  */
 
 int print_last_digit(int n)
 {
-	int l = n % 10;
-
-	_putchar('l');
-
+	int ld = n % 10;
+	if ((n < 0) || (n > 0))
+	_putchar(ld);
 	return (0);
 }

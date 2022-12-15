a8513d57c5b956590b0de5caf891b69bd960a836
Omegagrace <pro.egbebi@gmail.com>
2022-12-15 09:18:29 -0800

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,21 +5,17 @@
  * Return: 0
  */
 
+
 int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-	/**
-	 *while (i < 10)
-	 *{
-	 *	punchar(i);
-	 *}
-	 */
-
+	 /*while (i < 10)*/
+	 /*{*/
+	 /*	punchar(i);*/
+	 /*}*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
